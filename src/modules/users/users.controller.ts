209f637d34bb0bcaff234ea6c562311eaefe6077
Brayzonn <brayzonn@gmail.com>
2025-07-25 15:53:17 +0100

    added working postgree db

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(
+    @Body() userData: { email: string; firstName: string; lastName: string },
+  ) {
+    return await this.usersService.create(userData);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.usersService.findAll();
+  }
+}
