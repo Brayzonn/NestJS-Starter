72d40b7e2bc19bf0f5097456143e8a7e53be2b9f
Brayzonn <brayzonn@gmail.com>
2025-07-27 03:50:29 +0100

    validation and transform pipes added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { createCorsConfig } from './config/cors.config';
 import { setupHelmetAndCompression } from './config/helmet-compression.config';
+import { validationPipeOptions } from './config/validation.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +12,7 @@ async function bootstrap() {
 
   setupHelmetAndCompression(app);
   app.enableCors(createCorsConfig(configService));
-
+  app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
   await app.listen(parseInt(configService.get('PORT', '3000')));
 }
 bootstrap();
