f05864513c57d47097d08f0c102a5ed31b2cf6ad
Brayzonn <brayzonn@gmail.com>
2025-07-27 15:42:33 +0100

    added request-size limits config

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { AppModule } from './app.module';
 import { createCorsConfig } from './config/cors.config';
 import { setupHelmetAndCompression } from './config/helmet-compression.config';
 import { validationPipeOptions } from './config/validation.config';
+import { setupRequestSizeLimit } from './config/request-size.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  setupRequestSizeLimit(app, configService);
   setupHelmetAndCompression(app);
   app.enableCors(createCorsConfig(configService));
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
